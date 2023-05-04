10addd893acf39089ae8e4aebcb70216acac536c
jojomondag <josef.nobach@outlook.com>
2023-05-04 23:26:16 +0200

    Changes some names
    Changed some names for better readability!

---
+++ Data/PromptAnalysis.cs
@@ -0,0 +1,16 @@
+﻿namespace TLH.Data
+{
+    public static class PromptAnalysis
+    {
+        public static string TeacherFörhållningsätt = "Hej";
+        public static string Uppgiften = "På";
+        public static string ElevensInlämnadeUppgift = "Dig";
+
+        public static void CalculateTokens()
+        {
+            string text = TeacherFörhållningsätt + Uppgiften + ElevensInlämnadeUppgift;
+            //int tokenCount = CountTokens("");
+            //Console.WriteLine($"The number of tokens in the text is: {tokenCount}");
+        }
+    }
+}
